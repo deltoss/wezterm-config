53b8e1a368498948e909df604fb6e8f7006c74b2
EmTee <mtran0011@outlook.com>
2025-05-28 15:13:32 +1000

    Cleaned the wezterm.lua file of low value comments, and changed color scheme.

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,6 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
 config.default_prog = { "pwsh", "-NoLogo" }
 
 config.launch_menu = {
@@ -34,13 +30,11 @@ config.launch_menu = {
 	},
 }
 
--- For example, changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
 
--- or, changing the font size and color scheme.
-config.font_size = 14
-config.color_scheme = "AdventureTime"
+config.font_size = 15
+-- See: https://wezterm.org/colorschemes/index.html
+config.color_scheme = "Catppuccin Latte"
 
--- Finally, return the configuration to wezterm:
 return config
