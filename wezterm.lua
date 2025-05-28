c163987adca1a636b5ac4193cb385474c4cd32ae
deltoss <mtran0011@outlook.com>
2025-05-28 15:27:45 +1000

    Refactor to multiple files

--- wezterm.lua
+++ wezterm.lua
@@ -1,40 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.default_prog = { "pwsh", "-NoLogo" }
-
-config.launch_menu = {
-	{
-		label = "Neovim",
-		args = {
-			"nvim",
-		},
-	},
-	{
-		label = "lf",
-		args = {
-			"lf",
-		},
-	},
-	{
-		label = "LazyGit",
-		args = {
-			"LazyGit",
-		},
-	},
-	{
-		label = "LazyDocker",
-		args = {
-			"LazyDocker",
-		},
-	},
-}
-
-config.initial_cols = 120
-config.initial_rows = 28
-
-config.font_size = 15
--- See: https://wezterm.org/colorschemes/index.html
-config.color_scheme = "Catppuccin Latte"
+require("configs.launch").apply_to_config(config)
+require("configs.appearances").apply_to_config(config)
 
 return config
