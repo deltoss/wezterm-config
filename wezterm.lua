0b9832cfe296a3ac9420fe6afe794848b643cb79
deltoss <mtran0011@outlook.com>
2025-05-28 15:33:29 +1000

    Renamed appearances.lua to appearance.lua

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,6 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 require("configs.launch").apply_to_config(config)
-require("configs.appearances").apply_to_config(config)
+require("configs.appearance").apply_to_config(config)
 
 return config
