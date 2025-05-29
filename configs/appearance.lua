67c91cc6ef4eda9e372efb58afb198e2c8846a71
EmTee <mtran0011@outlook.com>
2025-05-30 09:41:28 +1000

    Adjusted colors for tab bar.

--- configs/appearance.lua
+++ configs/appearance.lua
@@ -9,6 +9,75 @@ function module.apply_to_config(config)
 	config.font_size = 15
 	-- See: https://wezterm.org/colorschemes/index.html
 	config.color_scheme = "Catppuccin Latte"
+
+	config.window_frame = {
+		-- The overall background color of the tab bar when
+		-- the window is focused
+		active_titlebar_bg = "#152238",
+
+		-- The overall background color of the tab bar when
+		-- the window is not focused
+		inactive_titlebar_bg = "#333333",
+	}
+
+	config.window_padding = {
+		left = "0cell",
+		right = "0cell",
+		top = "0cell",
+		bottom = "0cell",
+	}
+
+	config.colors = {
+		tab_bar = {
+			-- The active tab is the one that has focus in the window
+			active_tab = {
+				-- The color of the background area for the tab
+				bg_color = "#174574",
+				-- The color of the text for the tab
+				fg_color = "#c0c0c0",
+			},
+
+			-- Inactive tabs are the tabs that do not have focus
+			inactive_tab = {
+				bg_color = "#1b1032",
+				fg_color = "#808080",
+
+				-- The same options that were listed under the `active_tab` section above
+				-- can also be used for `inactive_tab`.
+			},
+
+			-- You can configure some alternate styling when the mouse pointer
+			-- moves over inactive tabs
+			inactive_tab_hover = {
+				bg_color = "#3b3052",
+				fg_color = "#909090",
+				italic = true,
+
+				-- The same options that were listed under the `active_tab` section above
+				-- can also be used for `inactive_tab_hover`.
+			},
+
+			-- The new tab button that let you create new tabs
+			new_tab = {
+				bg_color = "#152238",
+				fg_color = "#808080",
+
+				-- The same options that were listed under the `active_tab` section above
+				-- can also be used for `new_tab`.
+			},
+
+			-- You can configure some alternate styling when the mouse pointer
+			-- moves over the new tab button
+			new_tab_hover = {
+				bg_color = "#192843",
+				fg_color = "#909090",
+				italic = true,
+
+				-- The same options that were listed under the `active_tab` section above
+				-- can also be used for `new_tab_hover`.
+			},
+		},
+	}
 end
 
 -- Maximize window on start
