37028fc365afbf6c5ae7b1e0ae4df408b3efeb56
deltoss <mtran0011@outlook.com>
2025-05-30 09:54:12 +1000

    Added line to fix SSH issues with git

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.mux_enable_ssh_agent = false
+
 require("configs.launch").apply_to_config(config)
 require("configs.appearance").apply_to_config(config)
 require("plugins.smart-splits-nvim").apply_to_config(config)
