af0c4b7c6c984b8bd42fc4e13f44d330154e4c4a
deltoss <mtran0011@outlook.com>
2025-05-30 16:39:09 +1000

    Added key shortcut configurations

--- wezterm.lua
+++ wezterm.lua
@@ -5,6 +5,7 @@ config.mux_enable_ssh_agent = false
 
 require("configs.launch").apply_to_config(config)
 require("configs.appearance").apply_to_config(config)
+require("configs.keys").apply_to_config(config)
 require("plugins.smart-splits-nvim").apply_to_config(config)
 
 return config
