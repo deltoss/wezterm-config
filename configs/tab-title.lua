e80c0f777f664d9f0e30588310f87268b431e47f
EmTee <mtran0011@outlook.com>
2025-06-28 11:15:14 +1000

    Refactoring + terminal col size

--- configs/tab-title.lua
+++ configs/tab-title.lua
@@ -13,18 +13,18 @@ end
 -- title of the active pane in that tab.
 function tab_title(tab_info)
 	local title = tab_info.tab_title
+	local tabNumber = tab_info.tab_index + 1
 	-- if the tab title is explicitly set, take that
 	if title and #title > 0 then
-		return title
+		return tabNumber .. ": " .. title
 	end
 	-- Otherwise, use the title from the active pane
 	-- in that tab
-	return basename(tab_info.active_pane.title)
+	return tabNumber .. ": " .. basename(tab_info.active_pane.title)
 end
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local title = tab_title(tab)
-	return tab.tab_index + 1 .. ": " .. title
+	return tab_title(tab)
 end)
 
 return {
