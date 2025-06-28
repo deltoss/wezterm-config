5a3107cc58663bd82d9222418d42ecb534be5e49
deltoss <mtran0011@outlook.com>
2025-06-28 10:51:30 +1000

    Quick select configs + refactored file names

--- configs/launch.lua
+++ configs/launch.lua
@@ -1,55 +1,59 @@
+local wezterm = require("wezterm")
+
 local module = {}
 
 -- The suggested convention for making modules that update
 -- the config is for them to export an `apply_to_config`
 -- function that accepts the config object.
 function module.apply_to_config(config)
-	config.default_prog = { "pwsh", "-NoLogo" }
+	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+		config.default_prog = { "pwsh", "-NoLogo" }
 
-	config.launch_menu = {
-		{
-			label = "Default Shell",
-			args = {},
-		},
-		{
-			label = "Neovim",
-			args = {
-				"nvim",
+		config.launch_menu = {
+			{
+				label = "Default Shell",
+				args = {},
 			},
-		},
-		{
-			label = "Yazi",
-			args = { "pwsh", "-NoExit", "-NoLogo", "-c", "y" },
-		},
-		{
-			label = "LazyGit",
-			args = {
-				"LazyGit",
+			{
+				label = "Neovim",
+				args = {
+					"nvim",
+				},
 			},
-		},
-		{
-			label = "LazyDocker",
-			args = {
-				"LazyDocker",
+			{
+				label = "Yazi",
+				args = { "pwsh", "-NoExit", "-NoLogo", "-c", "y" },
 			},
-		},
-		{
-			label = "Neovim: Note Taking",
-			args = { "pwsh", "-NoLogo", "-NoProfile", "-c", 'cd (zoxide query "Note Taking"); nvim .' },
-		},
-		{
-			label = "Neovim: Neovim Configs",
-			args = { "pwsh", "-NoLogo", "-NoProfile", "-c", 'cd (zoxide query "Local\\nvim"); nvim .' },
-		},
-		{
-			label = "Neovim: Wezterm Configs",
-			args = { "pwsh", "-NoLogo", "-NoProfile", "-c", 'cd (zoxide query ".config\\wezterm"); nvim .' },
-		},
-		{
-			label = "Neovim: Powershell Profile",
-			args = { "pwsh", "-NoLogo", "-NoProfile", "-c", "nvim $PROFILE.CurrentUserAllHosts" },
-		},
-	}
+			{
+				label = "LazyGit",
+				args = {
+					"LazyGit",
+				},
+			},
+			{
+				label = "LazyDocker",
+				args = {
+					"LazyDocker",
+				},
+			},
+			{
+				label = "Neovim: Note Taking",
+				args = { "pwsh", "-NoLogo", "-NoProfile", "-c", 'cd (zoxide query "Note Taking"); nvim .' },
+			},
+			{
+				label = "Neovim: Neovim Configs",
+				args = { "pwsh", "-NoLogo", "-NoProfile", "-c", 'cd (zoxide query "Local\\nvim"); nvim .' },
+			},
+			{
+				label = "Neovim: Wezterm Configs",
+				args = { "pwsh", "-NoLogo", "-NoProfile", "-c", 'cd (zoxide query ".config\\wezterm"); nvim .' },
+			},
+			{
+				label = "Neovim: Powershell Profile",
+				args = { "pwsh", "-NoLogo", "-NoProfile", "-c", "nvim $PROFILE.CurrentUserAllHosts" },
+			},
+		}
+	end
 end
 
 return module
