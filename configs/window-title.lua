83225c75a4372f0cbf2bb1b01345fd451c5451ce
emtee <mtran0011@outlook.com>
2026-03-21 10:26:31 +1100

    Whitespace fixes

--- configs/window-title.lua
+++ configs/window-title.lua
@@ -1,11 +1,11 @@
 local wezterm = require("wezterm")
 
 wezterm.on("format-window-title", function()
-	local title = wezterm.mux.get_active_workspace()
-	title = title .. " → Domain: " .. wezterm.mux.get_domain():name()
-	return title
+  local title = wezterm.mux.get_active_workspace()
+  title = title .. " → Domain: " .. wezterm.mux.get_domain():name()
+  return title
 end)
 
 return {
-	apply_to_config = function(config) end,
+  apply_to_config = function(config) end,
 }
