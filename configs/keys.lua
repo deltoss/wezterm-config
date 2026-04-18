e9919613460923940dacd292274b282c7918b071
emtee <mtran0011@outlook.com>
2026-04-18 20:01:06 +1000

    Formatted keys.lua + added scrollback editing

--- configs/keys.lua
+++ configs/keys.lua
@@ -3,6 +3,7 @@ local act = wezterm.action
 
 local renameUtils = require("utils.rename")
 local terminalUtils = require("utils.terminal")
+require("utils.scrollback")
 
 local module = {}
 
@@ -10,282 +11,287 @@ local module = {}
 -- the config is for them to export an `apply_to_config`
 -- function that accepts the config object.
 function module.apply_to_config(config)
-	config.leader = {
-		key = "Insert",
-		mods = "",
-		timeout_milliseconds = 5000,
-	}
+  config.leader = {
+    key = "Insert",
+    mods = "",
+    timeout_milliseconds = 5000,
+  }
 
-	config.keys = {
-		-- Turn off problematic default key bindings
-		{ key = "p", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
-		{ key = "PageUp", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
-		{ key = "PageDown", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
-		{
-			key = "o",
-			mods = "LEADER",
-			action = act.ActivateTabRelative(-1),
-		},
-		{
-			key = "u",
-			mods = "LEADER",
-			action = act.ActivateTabRelative(1),
-		},
-		{
-			key = "p",
-			mods = "LEADER",
-			action = act.ActivateCommandPalette,
-		},
-		{
-			key = "c",
-			mods = "LEADER",
-			action = act.ActivateCopyMode,
-			-- See Copy Mode: https://wezterm.org/copymode.html
-		},
-		{
-			key = "s",
-			mods = "LEADER",
-			action = act.QuickSelect,
-		},
-		{
-			key = "/",
-			mods = "LEADER",
-			action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
-		},
-		{
-			key = ".",
-			mods = "LEADER",
-			action = act.SwitchWorkspaceRelative(1),
-		},
-		{
-			key = ",",
-			mods = "LEADER",
-			action = act.SwitchWorkspaceRelative(-1),
-		},
-		{
-			key = "w",
-			mods = "LEADER",
-			action = act.CloseCurrentPane({ confirm = false }),
-		},
-		{
-			key = "W",
-			mods = "LEADER|SHIFT",
-			action = act.CloseCurrentTab({ confirm = false }),
-		},
-		{
-			key = "f",
-			mods = "LEADER",
-			action = act.Search({ CaseSensitiveString = "" }),
-		},
-		{
-			key = "t",
-			mods = "LEADER",
-			action = act.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS|TABS|WORKSPACES" }),
-		},
-		{
-			key = "T",
-			mods = "LEADER|SHIFT",
-			action = act.ShowTabNavigator,
-		},
-		{
-			key = "UpArrow",
-			mods = "LEADER",
-			action = wezterm.action.SplitPane({
-				direction = "Up",
-				size = { Percent = 70 },
-			}),
-		},
-		{
-			key = "RightArrow",
-			mods = "LEADER",
-			action = wezterm.action.SplitPane({
-				direction = "Right",
-				size = { Percent = 30 },
-			}),
-		},
-		{
-			key = "DownArrow",
-			mods = "LEADER",
-			action = wezterm.action.SplitPane({
-				direction = "Down",
-				size = { Percent = 30 },
-			}),
-		},
-		{
-			key = "LeftArrow",
-			mods = "LEADER",
-			action = wezterm.action.SplitPane({
-				direction = "Left",
-				size = { Percent = 70 },
-			}),
-		},
-		{
-			key = "r",
-			mods = "LEADER",
-			action = renameUtils.promptRenameAction,
-		},
-		{
-			key = "z",
-			mods = "LEADER",
-			action = wezterm.action.TogglePaneZoomState,
-		},
-		{
-			key = "i",
-			mods = "LEADER",
-			action = terminalUtils.toggleTerminalAction,
-		},
-		{
-			key = ";",
-			mods = "LEADER",
-			action = terminalUtils.toggleTerminalAction,
-		},
-		{
-			key = "o",
-			mods = "ALT",
-			action = act.ActivateTabRelative(-1),
-		},
-		{
-			key = "u",
-			mods = "ALT",
-			action = act.ActivateTabRelative(1),
-		},
-		{
-			key = "PageDown",
-			mods = "ALT",
-			action = act.ActivateTabRelative(-1),
-		},
-		{
-			key = "PageUp",
-			mods = "ALT",
-			action = act.ActivateTabRelative(1),
-		},
-		{
-			key = "{",
-			mods = "SHIFT|ALT",
-			action = act.MoveTabRelative(-1),
-		},
-		{
-			key = "}",
-			mods = "SHIFT|ALT",
-			action = act.MoveTabRelative(1),
-		},
-		{
-			key = "o",
-			mods = "ALT|CTRL",
-			action = act.MoveTabRelative(-1),
-		},
-		{
-			key = "u",
-			mods = "ALT|CTRL",
-			action = act.MoveTabRelative(1),
-		},
-		{
-			key = "PageDown",
-			mods = "ALT|CTRL",
-			action = act.MoveTabRelative(-1),
-		},
-		{
-			key = "PageUp",
-			mods = "ALT|CTRL",
-			action = act.MoveTabRelative(1),
-		},
-		{
-			key = "p",
-			mods = "ALT",
-			action = act.ActivateCommandPalette,
-		},
-		{
-			key = "F1",
-			action = act.ActivateCommandPalette,
-		},
-		{
-			key = "c",
-			mods = "ALT",
-			action = act.ActivateCopyMode,
-			-- See Copy Mode: https://wezterm.org/copymode.html
-		},
-		{
-			key = "s",
-			mods = "ALT",
-			action = act.QuickSelect,
-		},
-		{
-			key = "/",
-			mods = "ALT",
-			action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
-		},
-		{
-			key = ".",
-			mods = "ALT",
-			action = act.SwitchWorkspaceRelative(1),
-		},
-		{
-			key = ",",
-			mods = "ALT",
-			action = act.SwitchWorkspaceRelative(-1),
-		},
-		{
-			key = "w",
-			mods = "ALT",
-			action = act.CloseCurrentPane({ confirm = false }),
-		},
-		{
-			key = "F4",
-			mods = "CTRL",
-			action = act.CloseCurrentTab({ confirm = false }),
-		},
-		{
-			key = "f",
-			mods = "ALT",
-			action = act.Search({ CaseSensitiveString = "" }),
-		},
-		{
-			key = "T",
-			mods = "ALT|SHIFT",
-			action = act.ShowTabNavigator,
-		},
-		{
-			key = "t",
-			mods = "ALT",
-			action = act.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS|TABS|WORKSPACES" }),
-		},
-		{
-			key = "r",
-			mods = "ALT",
-			action = renameUtils.promptRenameAction,
-		},
-		{
-			key = "z",
-			mods = "ALT",
-			action = wezterm.action.TogglePaneZoomState,
-		},
-		{
-			key = "i",
-			mods = "ALT",
-			action = terminalUtils.toggleTerminalAction,
-		},
-		{
-			key = ";",
-			mods = "ALT",
-			action = terminalUtils.toggleTerminalAction,
-		},
-	}
+  config.keys = {
+    -- Turn off problematic default key bindings
+    { key = "p", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
+    { key = "PageUp", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+    { key = "PageDown", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+    {
+      key = "o",
+      mods = "CTRL|SHIFT",
+      action = act.EmitEvent("trigger-editor-with-scrollback"),
+    },
+    {
+      key = "o",
+      mods = "LEADER",
+      action = act.ActivateTabRelative(-1),
+    },
+    {
+      key = "u",
+      mods = "LEADER",
+      action = act.ActivateTabRelative(1),
+    },
+    {
+      key = "p",
+      mods = "LEADER",
+      action = act.ActivateCommandPalette,
+    },
+    {
+      key = "c",
+      mods = "LEADER",
+      action = act.ActivateCopyMode,
+      -- See Copy Mode: https://wezterm.org/copymode.html
+    },
+    {
+      key = "s",
+      mods = "LEADER",
+      action = act.QuickSelect,
+    },
+    {
+      key = "/",
+      mods = "LEADER",
+      action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+    },
+    {
+      key = ".",
+      mods = "LEADER",
+      action = act.SwitchWorkspaceRelative(1),
+    },
+    {
+      key = ",",
+      mods = "LEADER",
+      action = act.SwitchWorkspaceRelative(-1),
+    },
+    {
+      key = "w",
+      mods = "LEADER",
+      action = act.CloseCurrentPane({ confirm = false }),
+    },
+    {
+      key = "W",
+      mods = "LEADER|SHIFT",
+      action = act.CloseCurrentTab({ confirm = false }),
+    },
+    {
+      key = "f",
+      mods = "LEADER",
+      action = act.Search({ CaseSensitiveString = "" }),
+    },
+    {
+      key = "t",
+      mods = "LEADER",
+      action = act.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS|TABS|WORKSPACES" }),
+    },
+    {
+      key = "T",
+      mods = "LEADER|SHIFT",
+      action = act.ShowTabNavigator,
+    },
+    {
+      key = "UpArrow",
+      mods = "LEADER",
+      action = wezterm.action.SplitPane({
+        direction = "Up",
+        size = { Percent = 70 },
+      }),
+    },
+    {
+      key = "RightArrow",
+      mods = "LEADER",
+      action = wezterm.action.SplitPane({
+        direction = "Right",
+        size = { Percent = 30 },
+      }),
+    },
+    {
+      key = "DownArrow",
+      mods = "LEADER",
+      action = wezterm.action.SplitPane({
+        direction = "Down",
+        size = { Percent = 30 },
+      }),
+    },
+    {
+      key = "LeftArrow",
+      mods = "LEADER",
+      action = wezterm.action.SplitPane({
+        direction = "Left",
+        size = { Percent = 70 },
+      }),
+    },
+    {
+      key = "r",
+      mods = "LEADER",
+      action = renameUtils.promptRenameAction,
+    },
+    {
+      key = "z",
+      mods = "LEADER",
+      action = wezterm.action.TogglePaneZoomState,
+    },
+    {
+      key = "i",
+      mods = "LEADER",
+      action = terminalUtils.toggleTerminalAction,
+    },
+    {
+      key = ";",
+      mods = "LEADER",
+      action = terminalUtils.toggleTerminalAction,
+    },
+    {
+      key = "o",
+      mods = "ALT",
+      action = act.ActivateTabRelative(-1),
+    },
+    {
+      key = "u",
+      mods = "ALT",
+      action = act.ActivateTabRelative(1),
+    },
+    {
+      key = "PageDown",
+      mods = "ALT",
+      action = act.ActivateTabRelative(-1),
+    },
+    {
+      key = "PageUp",
+      mods = "ALT",
+      action = act.ActivateTabRelative(1),
+    },
+    {
+      key = "{",
+      mods = "SHIFT|ALT",
+      action = act.MoveTabRelative(-1),
+    },
+    {
+      key = "}",
+      mods = "SHIFT|ALT",
+      action = act.MoveTabRelative(1),
+    },
+    {
+      key = "o",
+      mods = "ALT|CTRL",
+      action = act.MoveTabRelative(-1),
+    },
+    {
+      key = "u",
+      mods = "ALT|CTRL",
+      action = act.MoveTabRelative(1),
+    },
+    {
+      key = "PageDown",
+      mods = "ALT|CTRL",
+      action = act.MoveTabRelative(-1),
+    },
+    {
+      key = "PageUp",
+      mods = "ALT|CTRL",
+      action = act.MoveTabRelative(1),
+    },
+    {
+      key = "p",
+      mods = "ALT",
+      action = act.ActivateCommandPalette,
+    },
+    {
+      key = "F1",
+      action = act.ActivateCommandPalette,
+    },
+    {
+      key = "c",
+      mods = "ALT",
+      action = act.ActivateCopyMode,
+      -- See Copy Mode: https://wezterm.org/copymode.html
+    },
+    {
+      key = "s",
+      mods = "ALT",
+      action = act.QuickSelect,
+    },
+    {
+      key = "/",
+      mods = "ALT",
+      action = act.ShowLauncherArgs({ flags = "WORKSPACES" }),
+    },
+    {
+      key = ".",
+      mods = "ALT",
+      action = act.SwitchWorkspaceRelative(1),
+    },
+    {
+      key = ",",
+      mods = "ALT",
+      action = act.SwitchWorkspaceRelative(-1),
+    },
+    {
+      key = "w",
+      mods = "ALT",
+      action = act.CloseCurrentPane({ confirm = false }),
+    },
+    {
+      key = "F4",
+      mods = "CTRL",
+      action = act.CloseCurrentTab({ confirm = false }),
+    },
+    {
+      key = "f",
+      mods = "ALT",
+      action = act.Search({ CaseSensitiveString = "" }),
+    },
+    {
+      key = "T",
+      mods = "ALT|SHIFT",
+      action = act.ShowTabNavigator,
+    },
+    {
+      key = "t",
+      mods = "ALT",
+      action = act.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS|TABS|WORKSPACES" }),
+    },
+    {
+      key = "r",
+      mods = "ALT",
+      action = renameUtils.promptRenameAction,
+    },
+    {
+      key = "z",
+      mods = "ALT",
+      action = wezterm.action.TogglePaneZoomState,
+    },
+    {
+      key = "i",
+      mods = "ALT",
+      action = terminalUtils.toggleTerminalAction,
+    },
+    {
+      key = ";",
+      mods = "ALT",
+      action = terminalUtils.toggleTerminalAction,
+    },
+  }
 
-	for i = 1, 9 do
-		-- leader + number to activate that tab
-		table.insert(config.keys, {
-			key = tostring(i),
-			mods = "LEADER",
-			action = wezterm.action.ActivateTab(i - 1),
-		})
+  for i = 1, 9 do
+    -- leader + number to activate that tab
+    table.insert(config.keys, {
+      key = tostring(i),
+      mods = "LEADER",
+      action = wezterm.action.ActivateTab(i - 1),
+    })
 
-		-- ALT + number to activate that tab
-		table.insert(config.keys, {
-			key = tostring(i),
-			mods = "ALT",
-			action = wezterm.action.ActivateTab(i - 1),
-		})
-	end
+    -- ALT + number to activate that tab
+    table.insert(config.keys, {
+      key = tostring(i),
+      mods = "ALT",
+      action = wezterm.action.ActivateTab(i - 1),
+    })
+  end
 end
 
 return module
