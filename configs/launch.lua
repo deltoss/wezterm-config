c7264f00e6db818fdc510b66018cd027927069ea
emtee <mtran0011@outlook.com>
2026-06-19 16:04:07 +1000

    Adjusted default program to be zellij

--- configs/launch.lua
+++ configs/launch.lua
@@ -6,10 +6,14 @@ local module = {}
 -- the config is for them to export an `apply_to_config`
 -- function that accepts the config object.
 function module.apply_to_config(config)
-  if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    config.default_prog =
-      { "nu", "--no-config-file", "--no-std-lib", "~/.config/nushell/custom-commands/quick-launch.nu" }
+  if not os.getenv("ZELLIJ_CONFIG_DIR") then
+    local xdg = os.getenv("XDG_CONFIG_HOME")
+    if xdg then
+      config.default_prog = { "zellij", "--config-dir", xdg .. "/zellij", "--layout", "quick-launch" }
+      return
+    end
   end
+  config.default_prog = { "zellij" }
 end
 
 return module
